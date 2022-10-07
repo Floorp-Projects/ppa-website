b249b53f9894ad16ffdb5ad389be7d20133304f9
regularenthropy <roottotalhax@gmail.com>
2022-10-07 19:41:31 +0200

    index.html: add pop-up to indicate copied text

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function copied_popup() {
+  var button = document.getElementById("copy-button");
+  button.innerText = "Copied successfully";
+  button.style.color = "white";
+  delay(1000).then(() => (button.innerText = "📋"));
+}
+
+function copycmd() {
+  navigator.clipboard.writeText(
+    "curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg\nsudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'\nsudo apt update\nsudo apt install floorp"
+  );
+  copied_popup();
+}
